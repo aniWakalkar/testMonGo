f97e9d388ec2d7e64dc605cd417054e93b1f9529
aniWakalkar <aniket.wakalkar@caredose.com>
2024-05-21 22:06:58 +0530

    all done

--- src/server.js
+++ src/server.js
@@ -1,23 +1,4 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-// MongoDB connection URI
-const mongoURI = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@${process.env.LOCAL_HOST}/entertainment_app`;
-
-// Connect to MongoDB
-const db_connection = async () => {
-    try {
-      await mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log('MongoDB connected...');
-    } catch (err) {
-      console.error('MongoDB connection error:', err);
-      process.exit(1); // Exit process with failure
-    }
-};
-
+const db_connection = require('./config/Db_connect');
 db_connection()
 
 const express = require('express');
