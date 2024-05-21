6b1fd1cc840b14487f17bfd4ac15de4142e5a851
aniWakalkar <aniket.wakalkar@caredose.com>
2024-05-21 22:03:27 +0530

    all done

--- src/routes/Authentication.js
+++ src/routes/Authentication.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('./UsersSchema');
+const User = require('../models/UsersSchema');
 require('dotenv').config();
 
 
